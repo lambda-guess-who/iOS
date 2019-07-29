9842076a33f3f7758be02b565ff898705a7a1bd4
Steven <Sleyva1991@outlook.com>
2019-07-29 13:31:05 -0400

    Initial Commit

---
+++ GuessWho/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GuessWho
+//
+//  Created by Steven Leyva on 7/29/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
