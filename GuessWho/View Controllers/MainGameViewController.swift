1ea7b1cbb821610a5cad9bcdcaba121255817220
Steven <Sleyva1991@outlook.com>
2019-07-31 16:38:00 -0400

    Made MainGame storyboard, hooked everything up and started on gameover storyboard

---
+++ GuessWho/View Controllers/MainGameViewController.swift
@@ -0,0 +1,58 @@
+//
+//  MainGameViewController.swift
+//  GuessWho
+//
+//  Created by Steven Leyva on 7/31/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class MainGameViewController: UIViewController {
+    
+    @IBOutlet weak var catagoriesLabel: UILabel!
+    @IBOutlet weak var twitterTextView: UITextView!
+    @IBOutlet weak var userName1Label: UILabel!
+    @IBOutlet weak var userName2Label: UILabel!
+    @IBOutlet weak var userName3Label: UILabel!
+    @IBOutlet weak var twitterHandler1Label: UILabel!
+    @IBOutlet weak var twitterHandler2Label: UILabel!
+    @IBOutlet weak var twitterHandler3Label: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var commentLabel: UILabel!
+    @IBOutlet weak var retweetLabel: UILabel!
+    @IBOutlet weak var likeLabel: UILabel!
+    @IBOutlet weak var picture1Image: UIImageView!
+    @IBOutlet weak var picture2Image: UIImageView!
+    @IBOutlet weak var picture3Image: UIImageView!
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func closeButton(_ sender: Any) {
+    }
+    
+    @IBAction func choice1Tapped(_ sender: Any) {
+    }
+    
+    @IBAction func choice2Tapped(_ sender: Any) {
+    }
+    
+    @IBAction func choice3Tapped(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
